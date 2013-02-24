ea88b064ea6e65f1f3c9232259ddffe4238e65ed
aparup <aparup@gmail.com>
2013-02-24 08:01:46 -0800

    Initial commit for PostActivity Manager

--- routes.py
+++ routes.py
@@ -5,6 +5,7 @@ This file contains stat_routes
 from webapp2_extras.routes import RedirectRoute
 from web import handlers
 from web.joinhour import activity_handler, home_request_handler, join_handler, thank_you_handler, tip_handler
+from web.joinhour.post_activity_mgr_handlers import FeedbackEmailHandler
 
 
 secure_scheme = 'https'
@@ -15,7 +16,8 @@ _routes = [
     RedirectRoute('/tip/',tip_handler.TipHandler, name='tip', strict_slash=True),
     RedirectRoute('/tip/thankyou/', thank_you_handler.ThankYouHandler, name='thankyou', strict_slash=True),
     RedirectRoute('/activity/', activity_handler.ActivityHandler, name='activity', strict_slash=True),
-    RedirectRoute('/join/', join_handler, name='join', strict_slash=True)
+    RedirectRoute('/join/', join_handler, name='join', strict_slash=True),
+    RedirectRoute('/post_activity_mgr/', FeedbackEmailHandler, name='post_activity_mgr', strict_slash=True)
 ]
 
 def get_routes():
