b61fb9f4b0adb0c518c9a6d8c0870db89726cf9e
Aparup Banerjee <aparup@gmail.com>
2013-03-08 15:02:40 -0800

    Initial version of activity Manager

--- src/joinhour/models/activity.py
+++ src/joinhour/models/activity.py
@@ -2,17 +2,31 @@ __author__ = 'aparbane'
 from google.appengine.ext import ndb
 
 class Activity(ndb.Model):
+
+    INITIATED = 'INITIATED'
+    FORMING = 'FORMING'
+    EXPIRED = 'EXPIRED'
+    COMPLETE = 'COMPLETE'
+
     category = ndb.StringProperty()
-    min_number_of_people_to_join = ndb.StringProperty()
-    max_number_of_people_to_join = ndb.StringProperty()
+    min_number_of_people_to_join = ndb.IntegerProperty()
+    max_number_of_people_to_join = ndb.IntegerProperty()
     duration = ndb.StringProperty()
     expiration = ndb.StringProperty()
     note = ndb.StringProperty()
     date_entered = ndb.DateTimeProperty(auto_now_add=True)
     username = ndb.StringProperty()
     ip = ndb.StringProperty()
+    spots_remaining = ndb.IntegerProperty(default=max_number_of_people_to_join.split(" ")[0])
+    status = ndb.StringProperty(default=INITIATED,choices=[INITIATED,FORMING,EXPIRED,COMPLETE])
+
 
     @classmethod
     def query_activity(cls, ancestor_key):
         return cls.query(ancestor=ancestor_key).order(-cls.date_entered)
+
+
+
+
+
 
