4a54555269a63d521d01f8203b17d67f782616e9
Aparup Banerjee <aparup@gmail.com>
2013-03-08 18:44:59 -0800

    Initial version of activity Manager

---
+++ src/joinhour/models/interest.py
@@ -0,0 +1,14 @@
+__author__ = 'aparbane'
+__author__ = 'aparbane'
+from google.appengine.ext import ndb
+class Interest(ndb.Model):
+    interest = ndb.StringProperty()
+    date_entered = ndb.DateTimeProperty(auto_now_add=True)
+    username = ndb.StringProperty()
+    duration = ndb.IntegerProperty()
+    expiration = ndb.IntegerProperty()
+
+    @classmethod
+    def query_interest(cls, ancestor_key):
+        return cls.query(ancestor=ancestor_key).order(-cls.date_entered)
+
