9117e4c150fe4dcb88a23e731af0b3f080cf55b3
craig <craig.stevenson@gmail.com>
2013-03-16 19:06:47 -0400

    added the separate term sheet page.

---
+++ web/joinhour/terms_handler.py
@@ -0,0 +1,24 @@
+#google imports
+import webapp2
+from webapp2_extras.i18n import gettext as _
+
+# Boilerplate imports
+from boilerplate.lib.basehandler import BaseHandler
+from boilerplate import models
+import jinja2
+
+class TermsHandler(BaseHandler):
+    """
+    Handler for the terms and conditions
+    """
+    #orig
+    def get(self):
+#        if not self.user:
+#            return self.redirect_to('login')
+        params = {}
+        return self.render_template('terms.html', **params)
+
+    @webapp2.cached_property
+    def form(self):
+        return forms.StatForm(self)
+
