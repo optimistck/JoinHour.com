9ed82e54b8871d12b71fe70922b2e2d8de248927
Abin <ashahab@guidewire.com>
2013-04-02 23:32:54 -0700

    Push update working with rendering issues

--- web/joinhour/join_handler.py
+++ web/joinhour/join_handler.py
@@ -7,7 +7,7 @@ from boilerplate import models
 from src.joinhour.models.activity import Activity
 from src.joinhour.interest_manager import InterestManager
 from src.joinhour.activity_manager import ActivityManager
-
+from google.appengine.api import channel
 
 class JoinHandler(BaseHandler):
     """
@@ -31,10 +31,11 @@ class JoinHandler(BaseHandler):
                                                 username = user_info.username,note = self.form.note.data.strip(),ip = self.request.remote_addr,
                                                 min_number_of_people_to_join = self.form.min_number_of_people_to_join.data.strip(),
                                                 max_number_of_people_to_join = self.form.max_number_of_people_to_join.data.strip())
+                message = _("Your interest was registered successfully. We are searching for a match ... ")
             else:
                 InterestManager.create_interest(building_name=building_name,category=self.form.category.data.strip(),duration=self.form.duration.data.strip(),expiration = self.form.expiration.data.strip(),
                                                 username = user_info.username)
-            message = _("Your interest was registered successfully. We are searching for a match ... ")
+                message = _("Your interest was registered successfully. We are searching for a match ... ")
             self.add_message(message, 'success')
             return self.redirect_to('home')
 
