f6c9f3761cd90ee7809cde621b1ec10c28092d9c
Aparup Banerjee <aparup@gmail.com>
2013-04-28 22:33:07 -0700

    Liveboard : Refactoring - made activity and inetrest merge to a single model

--- web/joinhour/user_profile_handler.py
+++ web/joinhour/user_profile_handler.py
@@ -1,9 +1,6 @@
 __author__ = 'aparbane'
 from boilerplate import models
 from boilerplate.lib.basehandler import BaseHandler, user_required
-from src.joinhour.social_media import twitter
-
-
 
 
 class UserProfileHandler(BaseHandler):
@@ -15,11 +12,13 @@ class UserProfileHandler(BaseHandler):
         if username is not None:
             user = models.User.get_by_username(username)
             self.view.user = user
+            '''
             if user.twitter_screen_name is not None:
                 self.view.hasTwitter = True
                 twitter_data = twitter.get_friends_and_followers_count(user.twitter_screen_name)
                 self.view.twitter_friends_count = twitter_data['friends_count']
                 self.view.twitter_followers_count = twitter_data['followers_count']
+            '''
             if user.avatar is not  None:
                 self.view.hasAvatar = True
         return self.render_template('user_profile.html', **params)
